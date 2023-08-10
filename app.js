306ded044bc0c9ee4cf33f3417a42264dd5e7528
justin <mugishajuste1@gmail.com>
2023-08-10 13:15:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const section = document.querySelectorAll('section')
+const navLinks = document.querySelectorAll('header nav a')
+window.onscroll = ()=>{
+     section.forEach((sec)=>{
+        let top = window.scrollY
+        let offset = sec.offsetTop - 150
+        let heihgt = sec.offsetHeight
+        let id = sec.getAttribute('id')
+
+        if(top >= offset && top <offset +heihgt){
+            navLinks.forEach(links =>{
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*=' + id +']').classList.add('active') 
+            })
+        }
+    })
+
+    // sticky navabar 
+    let header = document.querySelector('header')
+    header.classList.toggle('sticky', window.scrollY > 100)
+}
