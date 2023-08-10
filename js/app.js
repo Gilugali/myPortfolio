8c8f7deba5ce6c680311193fd71f88e84110ff90
justin <mugishajuste1@gmail.com>
2023-08-10 16:27:35 +0200

    damn

--- js/app.js
+++ js/app.js
@@ -47,7 +47,7 @@ ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' })
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' })
 
 const typed = new Typed('.multiple-text', {
-    strings:['Whore', 'slut😅', 'b😌tch'],
+    strings:['frontend ', 'pianist', 'Backend Developer', 'fullstack Developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
