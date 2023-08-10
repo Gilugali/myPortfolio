0f1a125d22196d752ab97389ed5e687239b57de5
justin <mugishajuste1@gmail.com>
2023-08-10 15:01:07 +0200

    responsivenes and animation

--- js/app.js
+++ js/app.js
@@ -29,4 +29,28 @@ window.onscroll = ()=>{
     // sticky navabar 
     let header = document.querySelector('header')
     header.classList.toggle('sticky', window.scrollY > 100)
+    menuIcon.classList.remove('bx-x')
+    navbar.classList.remove('active')
+
 }
+
+
+ScrollReveal({ 
+    reset: true,
+    distance:'80px',
+    duration:2000,
+    delay:200 
+});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' })
+ScrollReveal().reveal('.home-img,.services-container, .porfolio-box, .contact form', { origin: 'bottom' })
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' })
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' })
+
+const typed = new Typed('.multiple-text', {
+    strings:['frontend Developer', 'pianist', 'Backend Developer', 'fullstack Developer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+
